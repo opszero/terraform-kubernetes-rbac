82fc4fae70d0b3844c823580624320bec393365a
Abhi Yerra <abhi@berkeley.edu>
2023-09-22 22:25:22 -0700

    Added base

---
+++ main.tf
@@ -0,0 +1,48 @@
+resource "kubernetes_role" "this" {
+  metadata {
+    name      = "default:ad-eks-admins"
+    namespace = "kube-system"
+    labels = {
+      sso_role = "eks-admins"
+    }
+  }
+
+  rule {
+    api_groups = ["*"]
+    resources  = ["*"]
+    verbs      = ["*"]
+  }
+}
+
+resource "kubernetes_role_binding" "this" {
+  metadata {
+    name      = "eks-admins-binding"
+    namespace = "kube-system"
+  }
+  role_ref {
+    api_group = "rbac.authorization.k8s.io"
+    kind      = "Role"
+    name      = kubernetes_role.this.metadata[0].name
+  }
+  subject {
+    kind      = "Group"
+    name      = kubernetes_role.this.metadata[0].name
+    api_group = "rbac.authorization.k8s.io"
+  }
+}
+
+resource "kubernetes_cluster_role_binding" "this" {
+  metadata {
+    name = "clusterrole-eks-admins-binding"
+  }
+  role_ref {
+    api_group = "rbac.authorization.k8s.io"
+    kind      = "ClusterRole"
+    name      = "cluster-admin"
+  }
+  subject {
+    kind      = "Group"
+    name      = "default:ad-eks-admins"
+    api_group = "rbac.authorization.k8s.io"
+  }
+}
